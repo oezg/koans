dde48fe2a6e4136abd88a06b7eed89782dd4e987
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-10-17 00:17:56 +0200

    clj

--- clojure/4clojure/src/rich4clojure/easy/problem_027.clj
+++ clojure/4clojure/src/rich4clojure/easy/problem_027.clj
@@ -5,25 +5,26 @@
 ;; By 4Clojure user: dbyrne
 ;; Difficulty: Easy
 ;; Tags: [seqs]
-;; 
+;;
 ;; Write a function which returns true if the given
 ;; sequence is a palindrome.
-;; 
-;; 
+;;
+;;
 ;; Hint: "racecar" does not equal '(\r \a \c \e \c \a \r)
 
-(def __ :tests-will-fail)
+
+
+(def __ #(= (reverse %) (seq %)))
 
 (comment
-  
-  )
+  (= (reverse "racecar") (seq "racecar")))
 
 (tests
-  (__ '(1 2 3 4 5)) :=
-  (__ "racecar") :=
-  (__ [:foo :bar :foo]) :=
-  (__ '(1 1 3 3 1 1)) :=
-  (__ '(:a :b :c)) :=)
+ (__ '(1 2 3 4 5)) :=
+ (__ "racecar") :=
+ (__ [:foo :bar :foo]) :=
+ (__ '(1 1 3 3 1 1)) :=
+ (__ '(:a :b :c)) :=)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/a9620760aad9da40c497f5750087a095
