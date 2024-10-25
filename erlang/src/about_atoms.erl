deb38ae9edd44f4ebfaf20bb4143dc7dacacfdd3
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-10-25 12:49:28 +0200

    atoms, binaries

--- erlang/src/about_atoms.erl
+++ erlang/src/about_atoms.erl
@@ -1,23 +1,19 @@
 -module(about_atoms).
--export([truth/0,
-         truth_is_not_always_boolean/0,
-         strings_are_not_atoms/0,
-         however_atoms_might_look_like_a_string/0,
-         atoms_are_the_basis_of_all_calls/0
-        ]).
+
+-export([truth/0, truth_is_not_always_boolean/0, strings_are_not_atoms/0,
+         however_atoms_might_look_like_a_string/0, atoms_are_the_basis_of_all_calls/0]).
 
 truth() ->
-    __ =:= true.
+    true =:= true.
 
 truth_is_not_always_boolean() ->
-    is_atom(true) =:= __.
+    is_atom(true) =:= true.
 
 strings_are_not_atoms() ->
-    is_atom("true") =:= __.
+    is_atom("true") =:= false.
 
 however_atoms_might_look_like_a_string() ->
-    is_atom('true') =:= __.
+    is_atom(true) =:= true.
 
 atoms_are_the_basis_of_all_calls() ->
-    apply(erlang, list_to_atom, ["erlang"]) =:= __.
-
+    apply(erlang, list_to_atom, ["erlang"]) =:= erlang.
--- erlang/src/about_boolean_expressions.erl
+++ erlang/src/about_boolean_expressions.erl
@@ -1,47 +1,39 @@
 -module(about_boolean_expressions).
--export([or_operator/0,
-         and_operator/0,
-         xor_operator/0,
-         not_operator/0,
-         being_strict/0,
-         being_not_so_strict/0,
-         special_operators_evaluate_only_what_is_necessary/0,
-         this_applies_to_or_as_well/0,
-         make_de_morgan_proud/0,
-         syntax_you_might_not_expect/0
-        ]).
+
+-export([or_operator/0, and_operator/0, xor_operator/0, not_operator/0, being_strict/0,
+         being_not_so_strict/0, special_operators_evaluate_only_what_is_necessary/0,
+         this_applies_to_or_as_well/0, make_de_morgan_proud/0, syntax_you_might_not_expect/0]).
 
 or_operator() ->
-    __ =:= true or false.
+    true =:= true or false.
 
 and_operator() ->
-    __ =:= true and false.
+    false =:= true and false.
 
 xor_operator() ->
-    __ =:= true xor false.
+    true =:= true xor false.
 
 not_operator() ->
-    __ =:= not true.
+    false =:= not true.
 
 being_strict() ->
-    StrictlyDefined = (42 =:= 42.0),
-    __ =:= StrictlyDefined.
+    StrictlyDefined = 42 =:= 42.0,
+    false =:= StrictlyDefined.
 
 being_not_so_strict() ->
-    NotSoStrictlyDefined = (42 == 42.0),
-    __ =:= NotSoStrictlyDefined.
+    NotSoStrictlyDefined = 42 == 42.0,
+    true =:= NotSoStrictlyDefined.
 
 special_operators_evaluate_only_what_is_necessary() ->
-    ShortCircuit = (true andalso false),
-    __ =:= ShortCircuit.
+    ShortCircuit = true andalso false,
+    false =:= ShortCircuit.
 
 this_applies_to_or_as_well() ->
-    ShortCircuit = (false orelse true),
-    __ =:= ShortCircuit.
+    ShortCircuit = false orelse true,
+    true =:= ShortCircuit.
 
 make_de_morgan_proud() ->
-    ((not true) orelse (__)) =:= (not (false andalso true)).
+    (not true orelse not false) =:= not (false andalso true).
 
 syntax_you_might_not_expect() ->
-    __ =:= (1 =< 2) and (3 /= 4).
-
+    true =:= (1 =< 2) and (3 /= 4).
