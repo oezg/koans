8dd21a0f7c66414daef6c0caa6b0570d0b31bfaf
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-10-27 12:21:11 +0100

    erl

--- erlang/src/about_guard_clauses.erl
+++ erlang/src/about_guard_clauses.erl
@@ -1,11 +1,10 @@
 -module(about_guard_clauses).
--export([truthfulness_is_necessary_for_guards/0,
-         there_exists_a_shorthand_for_and/0,
-         there_also_exists_a_shorthand_for_or/0
-        ]).
+
+-export([truthfulness_is_necessary_for_guards/0, there_exists_a_shorthand_for_and/0,
+         there_also_exists_a_shorthand_for_or/0]).
 
 truthfulness_is_necessary_for_guards() ->
-    is_this_argument_truthy(__).
+    is_this_argument_truthy(true).
 
 is_this_argument_truthy(X) when X ->
     get_here;
@@ -13,7 +12,7 @@ is_this_argument_truthy(_) ->
     not_here.
 
 there_exists_a_shorthand_for_and() ->
-    and_shorthand(__).
+    and_shorthand(0).
 
 and_shorthand(X) when is_integer(X), X < 2 ->
     get_here;
@@ -21,10 +20,9 @@ and_shorthand(_) ->
     not_here.
 
 there_also_exists_a_shorthand_for_or() ->
-    or_shorthand(__).
+    or_shorthand(0.0).
 
 or_shorthand(X) when is_integer(X); is_float(X) ->
     get_here;
 or_shorthand(_) ->
     not_here.
-
--- erlang/src/about_pattern_matching.erl
+++ erlang/src/about_pattern_matching.erl
@@ -1,29 +1,26 @@
 -module(about_pattern_matching).
--export([what_looks_like_assignment_might_not_be/0,
-         assignment_with_a_tuple/0,
-         branching_on_patterns/0,
-         sometimes_we_do_not_need_to_know_everything/0,
-         and_sometimes_we_dont_need_to_match_anything/0,
-         we_can_find_patterns_in_arguments/0
-        ]).
+
+-export([what_looks_like_assignment_might_not_be/0, assignment_with_a_tuple/0,
+         branching_on_patterns/0, sometimes_we_do_not_need_to_know_everything/0,
+         and_sometimes_we_dont_need_to_match_anything/0, we_can_find_patterns_in_arguments/0]).
 
 what_looks_like_assignment_might_not_be() ->
     Fruits = apple,
-    Fruits = __.
+    Fruits = apple.
 
 assignment_with_a_tuple() ->
     {X, Y} = {banana, apple},
-    (X =:= __) and (Y =:= apple).
+    (X =:= banana) and (Y =:= apple).
 
 branching_on_patterns() ->
     Fruits = {banana, apple},
     case Fruits of
         {banana, Item} ->
-            __ =:= Item
+            apple =:= Item
     end.
 
 sometimes_we_do_not_need_to_know_everything() ->
-    Fruits = {__, banana},
+    Fruits = {apple, banana},
     case Fruits of
         {apple, _} ->
             get_here
@@ -36,10 +33,10 @@ and_sometimes_we_dont_need_to_match_anything() ->
         _ ->
             Answer = anything
     end,
-    __ =:= Answer.
+    anything =:= Answer.
 
 we_can_find_patterns_in_arguments() ->
-    match_the_argument(__).
+    match_the_argument(bar).
 
 match_the_argument(foo) ->
     wrong;
@@ -47,4 +44,3 @@ match_the_argument(bar) ->
     right;
 match_the_argument(_) ->
     wronger.
-
