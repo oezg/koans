8d98171edcd12f83df3c60536eae0f6448a169a3
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-10-28 00:09:58 +0100

    dicts & maps

--- erlang/src/about_dicts.erl
+++ erlang/src/about_dicts.erl
@@ -1,23 +1,19 @@
 -module(about_dicts).
--export([dictionaries_give_the_definition_of_a_key/0,
-         append_assumes_something/0,
-         fetching_what_is_not_available_is_troublesome/0,
-         however_find_is_a_bit_safer/0,
-         find_reports_on_matches/0,
-         erasing_from_a_dict/0,
-         size_is_relative/0
-        ]).
+
+-export([dictionaries_give_the_definition_of_a_key/0, append_assumes_something/0,
+         fetching_what_is_not_available_is_troublesome/0, however_find_is_a_bit_safer/0,
+         find_reports_on_matches/0, erasing_from_a_dict/0, size_is_relative/0]).
 
 dictionaries_give_the_definition_of_a_key() ->
     Dict = dict:store(key, value, dict:new()),
-    __ =:= dict:fetch(key, Dict).
+    value =:= dict:fetch(key, Dict).
 
 append_assumes_something() ->
     Dict = dict:append(key, value, dict:new()),
-    __ =:= dict:fetch(key, Dict).
+    [value] =:= dict:fetch(key, Dict).
 
 fetching_what_is_not_available_is_troublesome() ->
-    __ =:= bad_fetch().
+    badarg =:= bad_fetch().
 
 bad_fetch() ->
     try dict:fetch(not_a_key, dict:new()) of
@@ -29,20 +25,21 @@ bad_fetch() ->
     end.
 
 however_find_is_a_bit_safer() ->
-    __ =:= dict:find(not_a_key, dict:new()).
+    error =:= dict:find(not_a_key, dict:new()).
 
 find_reports_on_matches() ->
     Dict = dict:store(key, value, dict:new()),
     {Report, value} = dict:find(key, Dict),
-    __ =:= Report.
+    ok =:= Report.
 
 erasing_from_a_dict() ->
     Dict = dict:store(key, value, dict:new()),
     NewDict = dict:erase(key, Dict),
-    __ =:= dict:find(key, NewDict).
+    error =:= dict:find(key, NewDict).
 
 size_is_relative() ->
     BuiltInFunctionSize = size(dict:new()),
-    DictionarySize = dict:size(dict:new()),
-    __ =:= (BuiltInFunctionSize =:= DictionarySize).
-
+    DictionarySize =
+        dict:size(
+            dict:new()),
+    false =:= (BuiltInFunctionSize =:= DictionarySize).
--- erlang/src/about_lists.erl
+++ erlang/src/about_lists.erl
@@ -1,48 +1,41 @@
 -module(about_lists).
--export([lists_are_not_homogeneous/0,
-         we_can_add/0,
-         we_can_also_take_away/0,
-         lists_define_delete/0,
-         just_how_many_are_deleted/0,
-         lists_have_heads/0,
-         lists_also_have_tails/0,
-         constructing_with_cons/0,
-         length_is_as_simple_as_it_seems/0,
-         lists_of_tuples_can_be_found_by_key/0
-        ]).
+
+-export([lists_are_not_homogeneous/0, we_can_add/0, we_can_also_take_away/0,
+         lists_define_delete/0, just_how_many_are_deleted/0, lists_have_heads/0,
+         lists_also_have_tails/0, constructing_with_cons/0, length_is_as_simple_as_it_seems/0,
+         lists_of_tuples_can_be_found_by_key/0]).
 
 lists_are_not_homogeneous() ->
-    NotJustFruits = [apple, banana, __, mango],
+    NotJustFruits = [apple, banana, 1, mango],
     Element = lists:nth(3, NotJustFruits),
     (Element < 2) and (Element > 0).
 
 we_can_add() ->
-    [apple, banana] ++ __.
+    [apple, banana] ++ [mango, orange].
 
 we_can_also_take_away() ->
-    [apple, banana] -- [__].
+    [apple, banana] -- [apple].
 
 lists_define_delete() ->
-    lists:delete(__, [apple, banana]).
+    lists:delete(apple, [apple, banana]).
 
 just_how_many_are_deleted() ->
-    __ =:= lists:delete(apple, [apple, banana, apple]).
+    [banana, apple] =:= lists:delete(apple, [apple, banana, apple]).
 
 lists_have_heads() ->
     [Head | Tail] = [apple, banana, mango],
-    __ =:= Head.
+    apple =:= Head.
 
 lists_also_have_tails() ->
     [Head | Tail] = [apple, banana, mango],
-    __ =:= Tail.
+    [banana, mango] =:= Tail.
 
 constructing_with_cons() ->
-    __ =:= [apple | [banana | [mango | [pear | []]]]].
+    [apple, banana, mango, pear] =:= [apple, banana, mango, pear].
 
 length_is_as_simple_as_it_seems() ->
-    __ =:= length([1, 2, 3]).
+    3 =:= length([1, 2, 3]).
 
 lists_of_tuples_can_be_found_by_key() ->
     Meals = [{breakfast, eggs}, {lunch, pasta}, {dinner, burrito}],
-    {lunch, _} = lists:keyfind(__, 2, Meals).
-
+    {lunch, _} = lists:keyfind(pasta, 2, Meals).
