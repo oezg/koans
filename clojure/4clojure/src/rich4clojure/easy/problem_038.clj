5b71a13514ee7b1f4686c0b7251f2fda84ef2df4
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-10-29 22:34:00 +0100

    max range

--- clojure/4clojure/src/rich4clojure/easy/problem_038.clj
+++ clojure/4clojure/src/rich4clojure/easy/problem_038.clj
@@ -5,22 +5,27 @@
 ;; By 4Clojure user: dbyrne
 ;; Difficulty: Easy
 ;; Tags: [core-functions]
-;; 
+;;
 ;; Write a function which takes a variable number of
 ;; parameters and returns the maximum value.
 
 (def restricted [max max-key])
 
-(def __ :tests-will-fail)
+(defn helper [acc [head & tail]]
+  (if (nil? head)
+    acc
+    (recur (if (< acc head) head acc) tail)))
+
+(defn __ [head & tail]
+  (helper head tail))
 
 (comment
-  
-  )
+  (__ 36 18 37 4591 61 381))
 
 (tests
-  (__ 1 8 3 4) := 8
-  (__ 30 20) := 30
-  (__ 45 67 11) := 67)
+ (__ 1 8 3 4) := 8
+ (__ 30 20) := 30
+ (__ 45 67 11) := 67)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/3d8cce63160543ce69b40bc041174b28
