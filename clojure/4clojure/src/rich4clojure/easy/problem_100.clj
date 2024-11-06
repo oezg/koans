1d56395f4db83439c6964349198ff6b28abca2ea
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-11-06 23:01:41 +0100

    lcm

--- clojure/4clojure/src/rich4clojure/easy/problem_100.clj
+++ clojure/4clojure/src/rich4clojure/easy/problem_100.clj
@@ -5,23 +5,33 @@
 ;; By 4Clojure user: dbyrne
 ;; Difficulty: Easy
 ;; Tags: [math]
-;; 
+;;
 ;; Write a function which calculates the least common
 ;; multiple. Your function should accept a variable number
 ;; of positive integers or ratios.
 
-(def __ :tests-will-fail)
+(defn gcd [a b]
+  (cond
+    (zero? b) a
+    (< a b) (recur b a)
+    :else (recur b (rem a b))))
 
-(comment
-  
-  )
+(defn lcm [a b]
+  (/ (* a b) (gcd a b)))
+
+(defn least-common-multiple [& nums]
+  (reduce lcm nums))
+
+(def __ least-common-multiple)
+
+(comment)
 
 (tests
-  (__ 2 3) := 6
-  (__ 5 3 7) := 105
-  (__ 1/3 2/5) := 2
-  (__ 3/4 1/6) := 3/2
-  (__ 7 5/7 2 3/5) := 210)
+ (__ 2 3) := 6
+ (__ 5 3 7) := 105
+ (__ 1/3 2/5) := 2
+ (__ 3/4 1/6) := 3/2
+ (__ 7 5/7 2 3/5) := 210)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/f9876ddb08600e7c90e1647e14efeeae
