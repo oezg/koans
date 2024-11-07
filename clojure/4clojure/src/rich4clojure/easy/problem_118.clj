85a0a5e982f4664ba8959ec9d9942cbd8b15a220
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-11-07 23:59:03 +0100

    map

--- clojure/4clojure/src/rich4clojure/easy/problem_118.clj
+++ clojure/4clojure/src/rich4clojure/easy/problem_118.clj
@@ -5,29 +5,34 @@
 ;; By 4Clojure user: semisight
 ;; Difficulty: Easy
 ;; Tags: [core-seqs]
-;; 
+;;
 ;; Map is one of the core elements of a functional
 ;; programming language. Given a function f and an input
 ;; sequence s, return a lazy sequence of (f x) for each
 ;; element x in s .
 
-(def restricted [map map-indexed mapcat for])
+;; (def restricted [map map-indexed mapcat for])
 
-(def __ :tests-will-fail)
+(defn select [f s]
+  (lazy-seq
+   (when-let [[h & t] (seq s)]
+     (cons (f h) (select f t)))))
+
+(def __ select)
 
 (comment
-  
-  )
+  (print "123")
+  (cons 3 nil))
 
 (tests
-  [3 4 5 6 7] :=
-   (__ inc [2 3 4 5 6])
-  (repeat 10 nil) :=
-   (__ (fn [_] nil) (range 10))
-  [1000000 1000001] :=
-   (->> (__ inc (range))
-        (drop (dec 1000000))
-        (take 2)))
+ [3 4 5 6 7] :=
+ (__ inc [2 3 4 5 6])
+ (repeat 10 nil) :=
+ (__ (fn [_] nil) (range 10))
+ [1000000 1000001] :=
+ (->> (__ inc (range))
+      (drop (dec 1000000))
+      (take 2)))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/fcd991cd17eb81a7e9a42d84a0cda89f
