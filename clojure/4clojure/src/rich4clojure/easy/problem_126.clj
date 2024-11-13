8f64d8d5a7f7f51f0d302cd0bf19490871ff8392
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-11-13 23:30:06 +0100

    nil

--- clojure/4clojure/src/rich4clojure/easy/problem_126.clj
+++ clojure/4clojure/src/rich4clojure/easy/problem_126.clj
@@ -5,18 +5,15 @@
 ;; By 4Clojure user: amcnamara
 ;; Difficulty: Easy
 ;; Tags: [fun brain-teaser]
-;; 
+;;
 ;; Enter a value which satisfies the following:
 
-(def __ :tests-will-fail)
-
-(comment
-  
-  )
+(def x nil)
+(comment)
 
 (tests
-  [x __] :=
-  (and (= (class x) x) x))
+ x :=
+ (and (= (class x) x) x))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/8b24bd0cf40098ec3875dde7ecf3fc5c
