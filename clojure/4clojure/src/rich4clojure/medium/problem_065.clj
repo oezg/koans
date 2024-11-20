e87c6b7433ba4cbd73609d151c0c70a71f34d5df
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-11-20 23:01:34 +0100

    collection type

--- clojure/4clojure/src/rich4clojure/medium/problem_065.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_065.clj
@@ -5,37 +5,43 @@
 ;; By 4Clojure user: amalloy
 ;; Difficulty: Medium
 ;; Tags: [seqs testing]
-;; 
+;;
 ;; Clojure has many sequence types, which act in subtly
 ;; different ways. The core functions typically convert
 ;; them into a uniform "sequence" type and work with them
 ;; that way, but it can be important to understand the
 ;; behavioral and performance differences so that you know
 ;; which kind is appropriate for your application.
-;; 
-;; 
+;;
+;;
 ;; Write a function which takes a collection and returns
 ;; one of :map, :set, :list, or :vector - describing the
 ;; type of collection it was given.
-;; 
+;;
 ;; You won't be allowed to inspect their class or use the
 ;; built-in predicates like list? - the point is to poke
 ;; at them and understand their behavior.
 
-(def restricted [class type Class vector? sequential? list? seq? map? set? instance? getClass])
+(def restricted [class type Class vector? sequential? list? seq? map? set? instance?])
 
-(def __ :tests-will-fail)
+(defn sequence-type [coll]
+  (let [e (conj (empty coll) [:a 1] [:b 2] [:b 2])]
+    (cond
+      (= 1 (:a e)) :map
+      (= 2 (count e)) :set
+      (= 1 (second (first e))) :vector
+      (= 2 (second (first e))) :list)))
 
-(comment
-  
-  )
+(def __ sequence-type)
+
+(comment)
 
 (tests
-  :map := (__ {:a 1, :b 2})
-  :list := (__ (range (rand-int 20)))
-  :vector := (__ [1 2 3 4 5 6])
-  :set := (__ #{10 (rand-int 5)})
-  [:map :set :vector :list] := (map __ [{} #{} [] ()]))
+ :map := (__ {:a 1, :b 2})
+ :list := (__ (range (rand-int 20)))
+ :vector := (__ [1 2 3 4 5 6])
+ :set := (__ #{10 (rand-int 5)})
+ [:map :set :vector :list] := (map __ [{} #{} [] ()]))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/b4436b33f455e68d322970d458830049
