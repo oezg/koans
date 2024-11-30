d1eb141a98e56c3f7f9329f89f53ba966d7c0271
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-11-30 21:53:58 +0100

    k-combinations

--- clojure/4clojure/src/rich4clojure/medium/problem_103.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_103.clj
@@ -15,25 +15,22 @@
 ;; The number of k-combinations for a sequence is equal
 ;; to the binomial coefficient .
 
-(defn k-combinations [k S]
+(defn k-combinations [k [h & t :as s]]
   (cond
-    (not (pos-int? k)) #{#{}}
-    (not (set? S)) (k-combinations k (set S))
-    (< (count S) k) #{#{}}
-    (empty? S) #{#{}}
-    (= (count S) k) S
-    :else (let [[h & t] S]
-            (set/union (k-combinations k t)
-                       (set
-                        (map #(conj % h)
-                             (k-combinations
-                              (dec k)
-                              t)))))))
+    (zero? k) #{#{}}
+    (> k (count s)) #{}
+    (empty? s) #{}
+    :else (set/union (k-combinations k t)
+                     (set (map #(conj % h) (k-combinations (dec k) t))))))
 
-(def __ :tests-will-fail)
+(def __ k-combinations)
 
 (comment
+  (k-combinations 2 #{3 4 5})
   (set/union #{7 8 9} #{1 2 3 7})
+  (set (concat #{1 2 3} #{5 6 1 7}))
+  (set/union #{1 2 3} #{5 6 1 7})
+  (set/union #{7 8} nil)
   (k-combinations 2 #{6 3 1 4})
   (into #{} #{})
   #{#{}})
