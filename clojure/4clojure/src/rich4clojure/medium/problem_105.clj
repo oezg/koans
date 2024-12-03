47dfe99b6c1c0eabb30e0f27427b8a11effe9980
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-12-03 23:34:43 +0100

    lazy searching

--- clojure/4clojure/src/rich4clojure/medium/problem_105.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_105.clj
@@ -5,23 +5,47 @@
 ;; By 4Clojure user: amalloy
 ;; Difficulty: Medium
 ;; Tags: [maps seqs]
-;; 
+;;
 ;; Given an input sequence of keywords and numbers, create
 ;; a map such that each key in the map is a keyword, and
 ;; the value is a sequence of all the numbers (if any)
 ;; between it and the next keyword in the sequence.
 
-(def __ :tests-will-fail)
+(defn identify-keys [s]
+  (loop [acc {}
+         [h & t] s
+         vs []
+         old nil]
+    (let [nex (if old (assoc acc old vs) acc)]
+      (cond
+        (nil? h) nex
+        (keyword? h) (recur nex t [] h)
+        :else (recur acc t (conj vs h) old)))))
+
+(defn keys-to-nums
+  "Return a map with each keyword from the given sequence as key
+   and numbers between it and the next keyword as value."
+  ([s] (if (empty? s) {} (keys-to-nums {} s nil [])))
+  ([acc [h & t] ky nums]
+   (let [res (if ky (assoc acc ky nums) acc)]
+     (cond
+       (keyword? h) (recur res t h [])
+       h (recur acc t ky (conj nums h))
+       :else res))))
+
+(def __ keys-to-nums)
 
 (comment
-  
-  )
+  (identify-keys [])
+  (identify-keys [:a 1])
+  (assoc {} nil [])
+  (identify-keys [:a 1 2 3 :b :c 4]))
 
 (tests
-  {} := (__ [])
-  {:a [1]} := (__ [:a 1])
-  {:a [1], :b [2]} := (__ [:a 1, :b 2])
-  {:a [1 2 3], :b [], :c [4]} := (__ [:a 1 2 3 :b :c 4]))
+ {} := (__ [])
+ {:a [1]} := (__ [:a 1])
+ {:a [1], :b [2]} := (__ [:a 1, :b 2])
+ {:a [1 2 3], :b [], :c [4]} := (__ [:a 1 2 3 :b :c 4]))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/da9a4c4197dc581cb9635fe8358bc62d
--- clojure/4clojure/src/rich4clojure/medium/problem_108.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_108.clj
@@ -5,25 +5,43 @@
 ;; By 4Clojure user: amalloy
 ;; Difficulty: Medium
 ;; Tags: [seqs sorting]
-;; 
+;;
 ;; Given any number of sequences, each sorted from
 ;; smallest to largest, find the smallest single number
 ;; which appears in all of the sequences. The sequences
 ;; may be infinite, so be careful to search lazily.
 
-(def __ :tests-will-fail)
+(defn smallest-common-number [& args]
+  (loop [seqs args]
+    (let [heads (map first seqs)
+          max-head (apply max heads)
+          pop-smallers (partial drop-while #(< % max-head))]
+      (if (apply = heads)
+        (first heads)
+        (recur (map pop-smallers seqs))))))
+
+(defn smallest-common
+  "Return the smallest common number in the given sorted sequences."
+  [& seqs]
+  (let [heads (map first seqs)
+        maxihead (apply max heads)
+        pop-too-smalls (partial drop-while #(< % maxihead))]
+    (if (apply = heads)
+      (first heads)
+      (recur (map pop-too-smalls seqs)))))
+
+(def __ smallest-common)
 
 (comment
-  
-  )
+  (smallest-common 4))
 
 (tests
-  3 := (__ [3 4 5])
-  4 := (__ [1 2 3 4 5 6 7] [0.5 3/2 4 19])
-  7 := (__ (range) (range 0 100 7/6) [2 3 5 7 11 13])
-  64 := (__ (map #(* % % %) (range)) ;; perfect cubes
-          (filter #(zero? (bit-and % (dec %))) (range)) ;; powers of 2
-          (iterate inc 20)))
+ 3 := (__ [3 4 5])
+ 4 := (__ [1 2 3 4 5 6 7] [0.5 3/2 4 19])
+ 7 := (__ (range) (range 0 100 7/6) [2 3 5 7 11 13])
+ 64 := (__ (map #(* % % %) (range)) ;; perfect cubes
+           (filter #(zero? (bit-and % (dec %))) (range)) ;; powers of 2
+           (iterate inc 20)))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/4a0485cfb64ebc165afeffc2406b5669
