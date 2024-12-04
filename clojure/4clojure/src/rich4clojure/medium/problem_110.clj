1bbea2d2e8996fe0e86aa6ff2488c85c62e0520b
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-12-04 23:08:46 +0100

    pronunciation of sequence

--- clojure/4clojure/src/rich4clojure/medium/problem_110.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_110.clj
@@ -5,7 +5,7 @@
 ;; By 4Clojure user: mlni
 ;; Difficulty: Medium
 ;; Tags: [seqs]
-;; 
+;;
 ;; Write a function that returns a lazy sequence of
 ;; "pronunciations" of a sequence of numbers. A
 ;; pronunciation of each element in the sequence consists
@@ -13,23 +13,33 @@
 ;; number itself. For example, [1 1] is pronounced as [2
 ;; 1] ("two ones"), which in turn is pronounced as [1 2 1
 ;; 1] ("one two, one one").
-;; 
+;;
 ;; Your function should accept an initial sequence of
 ;; numbers, and return an infinite lazy sequence of
 ;; pronunciations, each element being a pronunciation of
 ;; the previous element.
 
-(def __ :tests-will-fail)
+(defn pronunciation
+  "Return the pronunciation of a sequence of numbers"
+  [s]
+  (->> (partition-by identity s)
+       (mapcat #(vector (count %) (first %)))))
+
+(defn pronunciations
+  "Return an infinite lazy sequence of pronunciations of an initial sequence of numbers"
+  [s]
+  (rest (iterate pronunciation s)))
+
+(def __ pronunciations)
 
 (comment
-  
-  )
+  (apply concat (pronunciation [2 1])))
 
 (tests
-  [[1 1] [2 1] [1 2 1 1]] := (take 3 (__ [1]))
-  [3 1 2 4] := (first (__ [1 1 1 4 4]))
-  [1 1 1 3 2 1 3 2 1 1] := (nth (__ [1]) 6)
-  338 := (count (nth (__ [3 2]) 15)))
+ [[1 1] [2 1] [1 2 1 1]] := (take 3 (__ [1]))
+ [3 1 2 4] := (first (__ [1 1 1 4 4]))
+ [1 1 1 3 2 1 3 2 1 1] := (nth (__ [1]) 6)
+ 338 := (count (nth (__ [3 2]) 15)))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/f4169b69fdd707f14ae92e5b006ff4b0
