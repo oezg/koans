25025e1903820b82adb2076e59ccf637b169d7ab
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-12-05 23:08:04 +0100

    global take while lazy

--- clojure/4clojure/src/rich4clojure/medium/problem_112.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_112.clj
@@ -5,7 +5,7 @@
 ;; By 4Clojure user: ghoseb
 ;; Difficulty: Medium
 ;; Tags: [seqs]
-;; 
+;;
 ;; Create a function which takes an integer and a nested
 ;; collection of integers as arguments. Analyze the
 ;; elements of the input collection and return a sequence
@@ -13,27 +13,40 @@
 ;; includes all elements starting from the head whose sum
 ;; is less than or equal to the input integer.
 
-(def __ :tests-will-fail)
+(defn sequs-horribilis
+  "Take elements from the (s)equence while the sum is not
+  greater than the (n)umber maintaining the nested structure."
+  ([n s] (reverse (sequs-horribilis '() n s)))
+  ([acc n [h & t]]
+   (cond
+     (neg? n) acc
+     (nil? h) acc
+     (coll? h) (conj acc (sequs-horribilis n h))
+     (> h n) acc
+     :else (recur (conj acc h) (- n h) t))))
+
+(def __ sequs-horribilis)
 
 (comment
-  
-  )
+  (__ 7 [[1 2] 3 4])
+
+  :rcf)
 
 (tests
-  (__ 10 [1 2 [3 [4 5] 6] 7]) :=
-   '(1 2 (3 (4)))
-  (__ 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11]) :=
-   '(1 2 (3 (4 (5 (6 (7))))))
-  (__ 9 (range)) :=
-   '(0 1 2 3)
-  (__ 1 [[[[[1]]]]]) :=
-   '(((((1)))))
-  (__ 0 [1 2 [3 [4 5] 6] 7]) :=
-   '()
-  (__ 0 [0 0 [0 [0]]]) :=
-   '(0 0 (0 (0)))
-  (__ 1 [-10 [1 [2 3 [4 5 [6 7 [8]]]]]]) :=
-   '(-10 (1 (2 3 (4)))))
+ (__ 10 [1 2 [3 [4 5] 6] 7]) :=
+ '(1 2 (3 (4)))
+ (__ 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11]) :=
+ '(1 2 (3 (4 (5 (6 (7))))))
+ (__ 9 (range)) :=
+ '(0 1 2 3)
+ (__ 1 [[[[[1]]]]]) :=
+ '(((((1)))))
+ (__ 0 [1 2 [3 [4 5] 6] 7]) :=
+ '()
+ (__ 0 [0 0 [0 [0]]]) :=
+ '(0 0 (0 (0)))
+ (__ 1 [-10 [1 [2 3 [4 5 [6 7 [8]]]]]]) :=
+ '(-10 (1 (2 3 (4)))))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/dfcf8ebb019b5ab679b66d1f812edddf
--- clojure/4clojure/src/rich4clojure/medium/problem_114.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_114.clj
@@ -5,34 +5,55 @@
 ;; By 4Clojure user: amalloy
 ;; Difficulty: Medium
 ;; Tags: [seqs higher-order-functions]
-;; 
+;;
 ;; take-while is great for filtering sequences, but it
 ;; limited: you can only examine a single item of the
 ;; sequence at a time. What if you need to keep track of
 ;; some state as you go over the sequence?
-;; 
-;; 
+;;
+;;
 ;; Write a function which accepts an integer n, a
 ;; predicate p, and a sequence. It should return a lazy
 ;; sequence of items in the list up to, but not including,
 ;; the n th item that satisfies the predicate.
 
-(def __ :tests-will-fail)
+(defn global-take-while
+  "Take until the (n)th item in the (s)equence
+   that satisfies the (p)redicate."
+  ([n p s] (reverse (global-take-while '() n p s)))
+  ([acc n p [h & t]]
+   (cond
+     (nil? h) acc
+     (and (= 1 n) (p h)) acc
+     (p h) (recur (cons h acc) (dec n) p t)
+     :else (recur (cons h acc) n p t))))
+
+(defn take-until
+  "Take until the (n)th item in the (s)equence
+   that satisfies the (p)redicate."
+  [n p [h & t :as s]]
+  (when (seq s)
+    (let [n' (if (p h) (dec n) n)]
+      (when (pos? n')
+        (lazy-seq (cons h (take-until n' p t)))))))
+
+(def __ take-until)
 
 (comment
-  
-  )
+  (cons 6 nil)
+  (when-let [a (seq '(1 2 3))]
+    (println a)))
 
 (tests
-  [2 3 5 7 11 13] :=
-   (__ 4 #(= 2 (mod % 3))
-         [2 3 5 7 11 13 17 19 23])
-  ["this" "is" "a" "sentence"] :=
-   (__ 3 #(some #{\i} %)
-         ["this" "is" "a" "sentence" "i" "wrote"])
-  ["this" "is"] :=
-   (__ 1 #{"a"}
-         ["this" "is" "a" "sentence" "i" "wrote"]))
+ [2 3 5 7 11 13] :=
+ (__ 4 #(= 2 (mod % 3))
+     [2 3 5 7 11 13 17 19 23])
+ ["this" "is" "a" "sentence"] :=
+ (__ 3 #(some #{\i} %)
+     ["this" "is" "a" "sentence" "i" "wrote"])
+ ["this" "is"] :=
+ (__ 1 #{"a"}
+     ["this" "is" "a" "sentence" "i" "wrote"]))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/9d1067c73d424ab77b3b6f1941f3c7a9
