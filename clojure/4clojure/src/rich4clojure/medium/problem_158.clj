e905756b8f6b497b4465d59e27793c7941862b1f
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-01-07 23:13:29 +0100

    decurry

--- clojure/4clojure/src/rich4clojure/medium/problem_158.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_158.clj
@@ -5,36 +5,36 @@
 ;; By 4Clojure user: amcnamara
 ;; Difficulty: Medium
 ;; Tags: [partial-functions]
-;; 
+;;
 ;; Write a function that accepts a curried function of
 ;; unknown arity n. Return an equivalent function of n
 ;; arguments.
-;; 
+;;
 ;; You may wish to read this .
 
-(def __ :tests-will-fail)
+(defn decurry [f] (fn [& args] (reduce #(%1 %2) f args)))
 
-(comment
-  
-  )
+(def __ decurry)
+
+(comment)
 
 (tests
-  10 := ((__ (fn [a]
-             (fn [b]
-               (fn [c]
-                 (fn [d]
-                   (+ a b c d))))))
-       1 2 3 4)
-  24 := ((__ (fn [a]
-             (fn [b]
-               (fn [c]
-                 (fn [d]
-                   (* a b c d))))))
-       1 2 3 4)
-  25 := ((__ (fn [a]
-             (fn [b]
-               (* a b))))
-       5 5))
+ 10 := ((__ (fn [a]
+              (fn [b]
+                (fn [c]
+                  (fn [d]
+                    (+ a b c d))))))
+        1 2 3 4)
+ 24 := ((__ (fn [a]
+              (fn [b]
+                (fn [c]
+                  (fn [d]
+                    (* a b c d))))))
+        1 2 3 4)
+ 25 := ((__ (fn [a]
+              (fn [b]
+                (* a b))))
+        5 5))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/8bdf1dba5225cd748e4d53545f7f68f7
