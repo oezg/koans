f5eb0e14c57e034627ffbde85b82e92ebcc5810e
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-01-12 23:38:50 +0100

    balanced brackets

--- clojure/4clojure/src/rich4clojure/medium/problem_177.clj
+++ clojure/4clojure/src/rich4clojure/medium/problem_177.clj
@@ -5,7 +5,7 @@
 ;; By 4Clojure user: daowen
 ;; Difficulty: Medium
 ;; Tags: [parsing]
-;; 
+;;
 ;; When parsing a snippet of code it's often a good idea
 ;; to do a sanity check to see if all the brackets match
 ;; up. Write a function that takes in a string and returns
@@ -13,25 +13,37 @@
 ;; are properly paired and legally nested, or returns
 ;; falsey otherwise.
 
-(def __ :tests-will-fail)
+(defn brackets [[h & t :as stack] br]
+  (case br
+    (\( \{ \[) (cons br stack)
+    (\) \} \]) (case (str h br) ("()" "[]" "{}") t (reduced [h]))
+    stack))
+
+(defn balanced? [s]
+  (->> s (reduce brackets '()) empty?))
+
+(def __ balanced?)
 
 (comment
-  
-  )
+  (empty? (reduce #(cond
+                     (#{\( \{ \[} %2) (cons %2 %1)
+                     (#{\) \} \]} %2) (if (#{[\( \)] [\{ \}] [\[ \]]} [(first %1) %2]) (rest %1) (reduced [:unbalanced]))
+                     :else %1) '() "367[]]"))
+  :rcf)
 
 (tests
-  "This string has no brackets." :=
-  "class Test {
-      public static void main(String[] args) {
-        System.out.println(\"Hello world.\");
-      }
-    }" :=
-  (__ "(start, end]") :=
-  (__ "())") :=
-  (__ "[ { ] } ") :=
-  "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))" :=
-  (__ "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))") :=
-  (__ "[") :=)
+ (__ "This string has no brackets.") := true
+ (__ "class Test {
+        public static void main(String[] args) {
+          System.out.println(\"Hello world.\");
+        }
+      }") := true
+ (__ "(start, end]") := false
+ (__ "())") := false
+ (__ "[ { ] } ") := false
+ (__ "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))") := true
+ (__ "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))") := false
+ (__ "[") := false)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/6b8d50ee0811042bdc646dc9060037e8
